ff4be652377f0d5a3884a64b7bf0bc51cefe6ce5
TakenpaUser <100627977+TakenpaUser@users.noreply.github.com>
2022-04-25 15:39:57 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,46 +1,71 @@
 --script for roblox
 local i = 2
 
-local function math(b,c)
-if isnumber(b) and isnumber(c) then
- --math is started
-    if b == c then
-      print("1")
-      return 1
-      else
-      if b < c then
-        print("a < b. Calculator cant print")
-        else
-        local wat = c * i
-        if wat == b then
-          print(i)
-          return i
-          else
-          if wat > b then
-            local res = wat - b
-            print(i,res)
-            return i,res
-            else
-            if wat < b then
-              i +=1
-              math(b,c)
-              end
-          end
-          end
-        end
-      end
-    else
-    print("The number(s) invalid.Please type a number")
- end
-  end
-
 local function isnumber(a)
-  local ok,yes = pcall(function()
-      local result = a:1
-   end))
-if ok then
-return true
-  else
-  return false
+	local ok,yes = pcall(function()
+		local result = tonumber(a) / 1
+	end)
+	if ok then
+	return true
+else
+	return false
 end
- end
+end
+
+local function math(b,c)
+	if isnumber(b) and isnumber(c) then
+		--math is started
+		if b == c then
+			print("1")
+			return 1
+		else
+			if b < c then
+				print("a < b. Calculator cant print")
+			else
+				local wat = c * i
+				if wat == b then
+					print(i)
+					return i
+				else
+					if wat > b then
+						local res = wat - b
+						print(i,res)
+						return i,res
+					else
+						if wat < b then
+							i +=1
+							math(b,c)
+						end
+					end
+				end
+			end
+		end
+	else
+		print("The number(s) invalid.Please type a number")
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+math(4511,112)
